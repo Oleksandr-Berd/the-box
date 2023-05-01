907d8b08dd881c3e27d0e036ee5e03cdf5fc8577
Oleksandr-Berd <alex.berd86@gmail.com>
2023-05-01 11:39:50 +0300

    correct hero section by add div with buttons

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,30 @@
-import styles from "./Hero.module.css"
+import styles from "./Hero.module.css";
+
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 
 const Hero = () => {
-    return ( <section className={styles.heroSection}></section> );
-}
- 
+  return (
+    <section className={styles.heroSection}>
+      <div className={styles.heroCon}>
+        <div className={styles.heroTextCon}>
+          <h3 className={styles.heroTitle}>Feature Projects</h3>
+          <p className={styles.heroText}>
+            The National University of Architecture
+          </p>
+        </div>
+        <div className={styles.btnCon}>
+          <button className={styles.heroBtn}>
+            <AiOutlineArrowLeft />
+            Back
+          </button>
+          <button className={styles.heroBtn}>
+            Next
+            <AiOutlineArrowRight />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default Hero;
