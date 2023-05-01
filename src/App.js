b156163c68afba7ee258df586946f282f22a3de8
Oleksandr-Berd <alex.berd86@gmail.com>
2023-05-01 13:00:41 +0300

    made services section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Reputation from "./components/Reputation/Reputation";
 import About from './components/About/About';
+import Services from './components/ServicesSection/Services';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
         <Header />
         <Hero />
         <Reputation />
-        <About/>
+        <About />
+        <Services/>
       </Layout>
     </div>
   );
